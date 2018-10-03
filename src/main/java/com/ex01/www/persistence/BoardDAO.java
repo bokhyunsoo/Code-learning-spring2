f333841670a26853ff588d5ef2f44c7b835f0dd7
bok-PC\bok <bok_0120@naver.com>
2018-10-03 16:15:48 +0900

    Board 테스트 #1

---
+++ src/main/java/com/ex01/www/persistence/BoardDAO.java
@@ -0,0 +1,17 @@
+package com.ex01.www.persistence;
+
+import java.util.List;
+
+import com.ex01.www.domain.BoardVO;
+
+public interface BoardDAO {
+	public void create(BoardVO vo) throws Exception;
+	
+	public BoardVO read(Integer bno) throws Exception;
+	
+	public void update(BoardVO vo) throws Exception;
+	
+	public void delete(Integer bno) throws Exception;
+	
+	public List<BoardVO> listAll() throws Exception;
+}
