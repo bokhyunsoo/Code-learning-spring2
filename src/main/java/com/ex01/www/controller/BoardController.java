49df17fc3a1e3fa5a43d33eece5fa573161c5037
bok-PC\bok <bok_0120@naver.com>
2018-10-04 16:08:45 +0900

    페이징 처리를 위한 SQL

--- src/main/java/com/ex01/www/controller/BoardController.java
+++ src/main/java/com/ex01/www/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,12 +59,12 @@ public class BoardController {
 	}
 	
 	@RequestMapping("listPage")
-	public String listPage(Criteria cri, Model model) throws Exception {
+	public String listPage(@ModelAttribute("cri") Criteria cri, Model model) throws Exception {
 		logger.info(cri.toString());
 		model.addAttribute("list", service.listCriteria(cri));
 		PageMaker pageMaker = new PageMaker();
 		pageMaker.setCri(cri);
-		pageMaker.setTotalCount(13);
+		pageMaker.setTotalCount(service.listCountCriteria(cri));
 		model.addAttribute("pageMaker", pageMaker);
 		return "board/listPage";
 	}
@@ -98,4 +99,6 @@ public class BoardController {
 		rttr.addFlashAttribute("msg", "SUCCESS");
 		return "redirect:/board/listAll";
 	}
+	
+	
 }
