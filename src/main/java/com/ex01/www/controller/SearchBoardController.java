c4ec53da4f86eb18d7e96b0c32136dca9f2e6a2e
bok-PC\bok <bok_0120@naver.com>
2018-10-04 17:27:11 +0900

    검색처리

---
+++ src/main/java/com/ex01/www/controller/SearchBoardController.java
@@ -0,0 +1,34 @@
+package com.ex01.www.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ex01.www.domain.PageMaker;
+import com.ex01.www.domain.SearchCriteria;
+import com.ex01.www.service.BoardService;
+
+@Controller
+@RequestMapping("sboard")
+public class SearchBoardController {
+	private static final Logger logger = LoggerFactory.getLogger(SearchBoardController.class);
+	
+	@Autowired
+	private BoardService service;
+	
+	@RequestMapping(value="list", method=RequestMethod.GET)
+	public String listPage(@ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
+		logger.info(cri.toString());
+		model.addAttribute("list", service.listCriteria(cri));
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		pageMaker.setTotalCount(service.listCountCriteria(cri));
+		model.addAttribute("pageMaker", pageMaker);
+		return "sboard/list";
+	}
+}
